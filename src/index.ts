53b9a30d549196a5325bd55b1eaeb878f265f509
Artem <artemisia8192@gmail.com>
2024-08-30 21:11:28 +0200

    Rewritten server

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,23 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import { ApolloServer } from '@apollo/server';
-import resolvers from './graphql/resolvers';
-import typeDefs from './graphql/typeDefs';
-import context from './graphql/context';
-import type { NextRequest } from 'next/server';
+import dotenv from 'dotenv';
+dotenv.config();
 import { WebSocketServer } from 'ws';
 import { useServer } from 'graphql-ws/lib/use/ws';
-import { createServer } from 'http';
+import context from './graphql/context';
+import resolvers from './graphql/resolvers';
+import typeDefs from './graphql/typeDefs';
 import { makeExecutableSchema } from '@graphql-tools/schema';
-import express from 'express'
-import cors from 'cors'
-import { expressMiddleware } from '@apollo/server/express4'
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-
-
+ 
+const server = new WebSocketServer({
+  port: 4000,
+  path: '/graphql',
+});
+ 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
-const app = express()
-
-const httpServer = createServer(app);
-
-const apolloServer = new ApolloServer({
-  schema,
-	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
-});
-
-const wsServer = new WebSocketServer({
-	server: httpServer,
-	path: '/',
-});
-
-wsServer.on('connection', () => {
-	console.log('New WebSocket connection');
-});
-
-wsServer.on('error', (err: any) => {
-	console.error('WebSocket server error: ', err);
-});
-
-/* eslint-disable react-hooks/rules-of-hooks */
-useServer({ schema, context }, wsServer);
-
-const startServer = async () => {
-	await apolloServer.start()
-
-	const corsOptions = {
-		origin: (origin: any, callback: any) => {
-			if (!origin) {
-				return callback(null, '*')
-			}
-			callback(null, origin)
-		},
-		credentials: true,
-		optionsSuccessStatus: 200,
-	}
-
-	app.use(cors(corsOptions))
-	app.use(express.json())
-	app.use(expressMiddleware(apolloServer, { context }))
-
-	const port = 4000
-	await new Promise((resolve, reject) => httpServer.listen({ port }, () => resolve(undefined)))
-	console.log(`Server listens on port ${port}`)
-}
-
-startServer()
+useServer({ schema, context }, server);
+ 
+console.log('Listening to port 4000');
 
