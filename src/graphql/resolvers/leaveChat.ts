7767f043ac5216d309856120ff466224c6c41e93
Artem <artemisia8192@gmail.com>
2024-09-01 12:00:20 +0200

    Fixed bug: when user leaves chat now all his messages are deleted

--- src/graphql/resolvers/leaveChat.ts
+++ src/graphql/resolvers/leaveChat.ts
@@ -1,8 +1,10 @@
 import UserChatRelation from '../../mongooseModels/UserChatRelation'
+import Message from '../../mongooseModels/Message'
 
 
 export default async function leaveChat(_: any, { username, chatID }: { username: string; chatID: string }) {
 	await UserChatRelation.findOneAndDelete({ username, chatID })
+	await Message.deleteMany({ chatID, authorUsername: username })
 	return {
 		ok: true,
 		message: "Successfully leaved a chat.",
